53fa5775ffaa45bd451c122775ae2398151c641e
Yuntian Zheng <yuz451@ucsd.edu>
2018-03-01 10:51:35 -0800

    Submission

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,17 @@
 function initMap() {
-	// add your code here
+	var ucsd_itlng = {
+		lat: 32.878886,
+		lng:  -117.235926
+	};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 14,
+			center: ucsd_itlng
+		});
+        
+    var marker = new google.maps.Marker({
+	    position: ucsd_itlng,
+		map: map,
+		title: "UCSD"
+	});
 }
